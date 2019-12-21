83ad0d653f95911bd77c8a9a637f6e01956c75fe
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2019-12-21 17:23:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,25 @@
 class Dog
-  # code goes here
+  class Artist
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+ 
+  def add_song(song)
+    @songs << song
+  end
+ 
+  def songs
+    @songs
+  end
+end
+
+drake = Artist.new("Drake")
+drake.add_song("In My Feelings")
+drake.add_song("Hotline Bling")
+
+kiki = Song.new("In My Feelings", "hip-hop")
+
 end
