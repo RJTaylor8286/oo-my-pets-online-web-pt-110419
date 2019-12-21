4013a8bc0abaa978daaae1765d03bd48733118d0
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2019-12-21 17:33:56 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,21 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :pets 
+ 
+  def initialize(Owner)
+    @owner = owner
+  end
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  bill = Owner.new("Bill")
+  bill.owner
 end
+  bill = Name.new("Bill")
+  bill.name
+end
+
+bill.owner.name
+end
+
